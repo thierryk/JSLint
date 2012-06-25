fe79d36883de3ce4984a74d35b7fc89ffd78d94d
thierryk <thierry.koblentz@gmail.com>
2012-06-24 21:51:21 -0700

    Removed one curl option
    Followlocation is not supported on MT (remote server)

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
       curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
       curl_setopt($ch, CURLOPT_HEADER, 0);
 
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-
       $data = curl_exec($ch);
       curl_close($ch);
 
